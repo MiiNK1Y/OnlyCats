9a75b36a4735be782846ae6c78eb45f221ddf950
Bethina S.M <bethina.s.moen@gmail.com>
2024-10-14 13:56:30 +0200

    moved buttons

---
+++ controllers/buttons.js
@@ -0,0 +1,23 @@
+function viewCatalogueButton(){
+    // code here
+}
+
+function viewHomeButton(){
+    // code here
+}
+
+function viewProfileButton(){
+    // code here
+}
+
+function viewCatButton(){
+    // code here
+}
+
+function viewRatingCardButton() {
+    // code here
+}
+
+function viewNewCatCardButton(){
+    // code here
+}
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,28 +1,3 @@
-// buttons ------------------------------------------------
-
-function viewCatalogueButton(){
-    // code here
-}
-
-function viewHomeButton(){
-    // code here
-}
-
-function viewProfileButton(){
-    // code here
-}
-
-function viewCatButton(){
-    // code here
-}
-
-function viewRatingCardButton() {
-    // code here
-}
-
-function viewNewCatCardButton(){
-    // code here
-}
 
 // functionality ------------------------------------------
 
