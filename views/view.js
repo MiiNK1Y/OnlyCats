82afdb48c5b07e66aa98076596cb69b2926315af
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-10-15 13:03:44 +0200

    added main function and comments

--- views/view.js
+++ views/view.js
@@ -4,3 +4,10 @@ function updateView() {
         ${viewNavigationBar()}
     `;
 }
+
+function main() {
+    // here we get the content to show,
+    // based on what page we are viewing.
+    
+    // logic goes here to check what to show.
+}
