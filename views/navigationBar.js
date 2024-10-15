ef0081b494459a537ba4353bd6a345448dc06863
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-10-15 14:10:38 +0200

    added image directory, completed navbar

--- views/navigationBar.js
+++ views/navigationBar.js
@@ -1,13 +1,31 @@
+function viewNavigationBar(){
+    html = /*HTML*/`
+        <div id="navbar">
+            <button class="home">Hjem</button>
+            <button class="catalogue">Kattalog</button>
+            <input id="search" type="text" placeholder="Søk" />
+            ${userNavbar()}
+        </div>
+    `;
 
+    return html;
+}
 
-function viewNavBar(){
-    model.app.html.innerHTML = /*HTML*/`
-    <div id="navbar">
-        <button class="home">Hjem</button>
-        <button class="catalogue">Kattalog</button>
-        <input id="search" type="text" />
-        <button class="login">Logg på</button>
-        <button class="new_user">Lag ny bruker</button>
-    </div>
-`;
+function userNavbar() {
+    user = model.app.currentUser
+
+    if (user == null) {
+        html = /*HTML*/`
+            <button class="login">Logg på</button>
+            <button class="new_user">Lag ny bruker</button>
+        `;
+    } else {
+        userPicture = model.data.user[model.app.currentUser].photo
+        html = /*HTML*/`
+            <img src="${userPicture}" />
+            <button>profile</button>
+        `;
+    }
+
+    return html;
 }
