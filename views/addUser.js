abbeab334ff3e944fa905d9ed66e0c55b81c4e61
Kamillaaks <kammi.ar@hotmail.com>
2024-10-15 12:38:18 +0200

    Update addUser.js

---
+++ views/addUser.js
@@ -0,0 +1,26 @@
+
+// KAMILLA
+
+updateAddUserView()
+
+function updateAddUserView() {
+    model.app.html.innerHTML = /* HTML */`
+        <div class="addUserContainer">
+            <div>Lag ny bruker</div>
+            <div class="addUserbox">
+                <div>Brukernavn:</div>
+                <div><input placeholder="Ditt brukernavn"/></div>
+                <br>
+                <div>Mailadresse:</div>
+                <div><input placeholder="Din mailadresse"/></div>
+                <br>
+                <div>Passord:</div>
+                <div><input placeholder="Ditt passord"/></div>
+                <br>
+                <div><input placeholder="Gjenta passord"/></div>
+                <br>
+            </div>
+            <div> <button onclick="addUser()">Lag ny bruker</button> </div>
+        </div>
+    `;
+}
