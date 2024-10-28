7ae11cb6d881b7db7e3f907341f4609f34ff54a5
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-10-28 11:00:16 +0100

    added median-rating algo for cats in model

---
+++ controllers/catRating.js
@@ -0,0 +1,23 @@
+function getMedian(ratings) {
+    const rating = Object.values(ratings);
+    let num = 0;
+    for (const rate of rating) {
+        num += (rate.ratingGiven);
+    }
+
+    const median = num / ratings.length;
+
+    return median;
+}
+
+function setRatings() {
+    let cats = Object.values(model.data.cat);
+    for (const cat of cats) {
+        cat.rating = getMedian(cat.givenRatings);
+        console.log(cat.name, cat.rating);
+    }
+}
+
+window.onload = () => {
+    setRatings();
+}
