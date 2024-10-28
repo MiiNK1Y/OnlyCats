d89584a8bb7ecfd2045b56957621dd5a95cfa23e
William B <william.buwembo21@gmail.com>
2024-10-28 14:58:21 +0100

    add user

--- controllers/addUser.js
+++ controllers/addUser.js
@@ -1,27 +1,58 @@
 function addNewUser() {
+    if (validateInput()) {
+        createAndAddUser();
+        resetInputs();
+        model.app.currentPage = "add user"; //?
+    }
+    updateView();
+}
+
+function createAndAddUser() {
+    let signUp = model.input.signUp;
+    let newUser = {
+        username: signUp.username,
+        id: model.data.user.length + 1,
+        email: signUp.email,
+        password: signUp.password,
+    };
+    model.data.user.push(newUser);
+}
+
+function validatePassword() {
+    let signUp = model.input.signUp;
+
+    if (signUp.password === signUp.repeatPassword) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateSignUpFields() {
     let signUp = model.input.signUp;
 
     if (!signUp.username || !signUp.email || !signUp.password || !signUp.repeatPassword) {
-        // Husk å fylle inn alle felter
-        return;
+        return true;
+    } else {
+        return false;
     }
-    if (signUp.password === signUp.repeatPassword) {
-        let newUser = {
-            username: signUp.username,
-            id: model.data.user.length + 1,
-            email: signUp.email,
-            password: signUp.password,
-        };
-        model.data.user.push(newUser);
+}
 
-        signUp.username = "";
-        signUp.email = "";
-        signUp.password = "";
-        signUp.repeatPassword = "";
-
-        model.app.currentPage = "add user";
-        updateView();
+function validateInput() {
+    if (validateSignUpFields()) {
+        //Husk å fylle inn alle felter
+        return false;
+    } else if (validatePassword()) {
+        return true;
     } else {
         // ikke samme passord
+        return false;
     }
+}
+
+function resetInputs() {
+    model.input.signUp.username = "";
+    model.input.signUp.email = "";
+    model.input.signUp.password = "";
+    model.input.signUp.repeatPassword = "";
 }
