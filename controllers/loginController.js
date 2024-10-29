6abfbe29a08b7861f43710533b633a6e0e039796
Kamillaaks <kammi.ar@hotmail.com>
2024-10-29 13:02:51 +0100

    Added loginController and changes to loginView

---
+++ controllers/loginController.js
@@ -0,0 +1,20 @@
+function signIn() {
+    let user = getUserByUsername(model.input.signIn.username)
+    if(user != undefined && user.password === model.input.signIn.password) {
+        model.app.currentPage = 'home'
+        updateView();
+    }
+    else {
+        model.app.errorMsg = 'Feil brukernavn eller passord';
+        // updateView();
+    }
+}
+
+function getUserByUsername(username) {
+    let users = model.data.user;    
+    for(let i = 0; i < users.length; i++) {
+        if (users[i].username == username) {
+            return users[i]
+        }
+    }
+}
--- views/loginView.js
+++ views/loginView.js
@@ -4,13 +4,14 @@ function loginView(){
             <h1>Login</h1>
             <div class="login_box">
                 <h2>Brukernavn:</h2>
-                <input placeholder="Ditt brukernavn"/>
+                <input placeholder="Ditt brukernavn" onchange = "model.input.signIn.username = this.value"/>
                 <h2>Passord:</h2>
-                <input type="password" placeholder="Ditt passord"/>
+                <input type="password" placeholder="Ditt passord" onchange = "model.input.signIn.password = this.value" />
+                <p>${model.app.errorMsg}</p>
             </div>
             <button onclick="signIn()">Logg inn</button>
         </div>
     `;
-
+    
     return html;
 }
