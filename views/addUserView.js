ba9738efe0b8094ca8f62cff3196ee28ce752a95
William B <william.buwembo21@gmail.com>
2024-10-30 10:11:33 +0100

    added default values on addUserView

--- views/addUserView.js
+++ views/addUserView.js
@@ -3,15 +3,37 @@ function addUserView() {
         <div class="addUserContainer">
             <div>Lag ny bruker</div>
             <div class="addUserbox">
+
                 <div>Brukernavn:</div>
-                <input placeholder="Ditt brukernavn" onchange="model.input.signUp.username = this.value" />
+                <input
+                    placeholder="Ditt brukernavn"
+                    onchange="model.input.signUp.username = this.value"
+                    value="${model.input.signUp.username ?? ""}"
+                />
+
                 <div>Mailadresse:</div>
-                <input placeholder="Din mailadresse" onchange="model.input.signUp.email = this.value" />
+                <input 
+                    placeholder="Din mailadresse" 
+                    onchange="model.input.signUp.email = this.value" 
+                    value="${model.input.signUp.email ?? ""}" 
+                />
+
                 <div>Passord:</div>
-                <input placeholder="Ditt passord" onchange="model.input.signUp.password = this.value" />
-                <input placeholder="Gjenta passord" onchange="model.input.signUp.repeatPassword = this.value" />
+                <input 
+                    placeholder="Ditt passord"
+                    onchange="model.input.signUp.password = this.value"
+                    value="${model.input.signUp.password ?? ""}"
+                />
+                <input
+                    placeholder="Gjenta passord"
+                    onchange="model.input.signUp.repeatPassword = this.value"
+                    value="${model.input.signUp.repeatPassword ?? ""}"
+                />
+
             </div>
-            <div><button onclick="addNewUser()">Lag ny bruker</button></div>
+            <div>
+                <button onclick="addNewUser()">Lag ny bruker</button>
+            </div>
             <div>${model.app.errorMessage}</div>
         </div>
     `;
