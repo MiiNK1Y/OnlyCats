e8e9421ce5145b0f70a3f31d46fac4fba491c569
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-11-04 12:40:46 +0100

    re-added navigationBarView.js

---
+++ views/navigationBarView.js
@@ -0,0 +1,33 @@
+function viewNavigationBar() {
+    html = /*HTML*/`
+        <div id="navbar">
+            <button class="home" onclick="homePage()">Hjem</button>
+            <button class="catalogue" onclick="viewCataloguePage()">Kattalog</button>
+            <input id="search" type="text" placeholder="Søk" />
+            ${userNavbar()}
+        </div>
+    `;
+
+    return html;
+}
+
+function userNavbar() {
+    let user = model.app.currentUser;
+
+    if (user == null) {
+        html = /*HTML*/`
+            <button class="login" onclick="viewLoginPage()">Logg på</button>
+            <button class="new_user" onclick="viewNewUserPage()">Lag ny bruker</button>
+        `;
+    } else {
+        userPicture = model.data.user[model.app.currentUser].photo;
+        userIndex = model.data.user.findIndex(a => a.id === user);
+        userPicture = model.data.user[user].photo;
+        html = /*HTML*/`
+            <img src="${userPicture}" />
+            <button onclick="viewProfilePage()">Profil</button>
+        `;
+    }
+
+    return html;
+}
