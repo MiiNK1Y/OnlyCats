2bca030f0f3ba065744641e645d418bc5e9a27ea
Henrik005 <henrik.mathias.brathen@gmail.com>
2024-11-05 13:18:28 +0100

    Changed name to be norwegian

--- views/profileView.experimental.js
+++ views/profileView.experimental.js
@@ -12,7 +12,7 @@ function viewProfile() {
                     </div>
                     <div> <button onclick="editProfile()"> Rediger profil </button> </div>
                     <div> <button onclick="addCat()"> Legg til ny katt </button> </div>
-                    <div> <button onclick="logOut()">Log Out </button> </div>
+                    <div> <button onclick="logOut()">Logg Ut </button> </div>
                 </div>
 
                 <div class="vertical_seperator"></div>
